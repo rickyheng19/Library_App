f34de1ae4ef2f59babac960df1f3ca09cf2a0edb
Kenna Heng <rickyheng19@gmail.com>
2023-12-04 05:25:06 -0600

    added author list get request

--- src/main/java/com/example/Library/Controller/AuthorController.java
+++ src/main/java/com/example/Library/Controller/AuthorController.java
@@ -6,9 +6,13 @@ import com.example.Library.domain.Entities.AuthorEntity;
 import com.example.Library.mappers.Mapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -28,4 +32,13 @@ public class AuthorController {
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
         return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
+
+    @GetMapping(path = "/authors")
+    public List<AuthorDTO> listAuthors() {
+        List<AuthorEntity> authors = authorService.findAll();
+        return authors.stream()
+                .map(authorMapper::mapTo)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/example/Library/Services/AuthorService.java
+++ src/main/java/com/example/Library/Services/AuthorService.java
@@ -3,7 +3,11 @@ package com.example.Library.Services;
 import com.example.Library.domain.Entities.AuthorEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity authorEntity);
+
+    List<AuthorEntity> findAll();
 }
